70ffd0cceeef95423628b93fc2a4ad7b5c1080c5
WaseemAzam901 <getwaseemazam@gmail.com>
2024-07-24 22:51:43 +0400

    Working On Form Submission

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,9 +1,14 @@
 'use client'
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
 const AddTodo = () => {
   const [todo, setTodo] = useState("")
+  const handleFormSubmit = (e:FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // handleAddTodo(todo);
+    setTodo("");
+  }
   return (
-    <form action="">
+    <form action="" onSubmit={handleFormSubmit}>
       <input type="text" placeholder='Write your todo' value={todo} onChange={(event)=> setTodo(event.target.value)} className='border '/>
       <button type='submit'>ADD</button>
     </form>
