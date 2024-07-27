3dd4b9ac81a29a37c1ddeb8d535643416a94e2c0
WaseemAzam901 <getwaseemazam@gmail.com>
2024-07-27 22:56:56 +0400

    USeContext Pending

--- src/store/todos.tsx
+++ src/store/todos.tsx
@@ -1,25 +1,42 @@
 import { createContext, ReactNode, useState } from 'react'
 
+
+export type Todo = {
+    id:string;
+    task:string;
+    completed:boolean;
+    createdAt: Date;
+}
+
+export type TodosContext = {
+    todos: Todo[];
+    handleAddTodo: (task: string) => void;
+}
+
 // Creating Context
-export const todosContext = createContext(null);
+export const todosContext = createContext<TodosContext | null>(null);
 
 //Creating Provider Function
-export const todosProvider =(children:{children:ReactNode}) => {
+export const TodosProvider =(children:{children:ReactNode}) => {
 
-    const [todos, setTodos] = useState([])
+    const [todos, setTodos] = useState<Todo[]>([])
 
     const handleAddTodo = (task:string) => {
     
         setTodos((prev)=> {
-            const newTodos = [{
+            const newTodos:Todo[] = [{
                 id: Math.random.toString(),
                 task,
-                complete:false,
-                createdAt: new Date();
+                completed:false,
+                createdAt: new Date()
 
-            }]
-            ...prev
+            },
+            ...prev]
+            return newTodos;
         })
-        return newTodos;
+        
     }
+    <todosContext.Provider value={{todos, handleAddTodo}}>
+        {children}
+    </todosContext.Provider>
 }
